0e39ef417e7791e0258f491ca0a2d41ac4e898d3
heo-it <yejin097@naver.com>
2022-09-22 19:42:00 +0900

    Feat: go함수 구현

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,15 @@
+// 인자로 전달된 함수를 연속적으로 실행하여 얻은 값을 리턴하는 함수
+const go = (...args) => reduce((a, f) => f(a), args); 
+
+/**
+ go(
+  0,
+  a => a + 1,
+  a => a + 10,
+  a => a + 100
+);
+ */
+
 const map = (f, iter) => {
 	let res = [];
 	for (const a of iter) {
